089230c798cb700419bbf42c6640d04a262e5e9d
Niklas Olofsson <niklas.olofsson@olif.nu>
2016-08-20 14:12:40 +0200

    Renamed client to BannerzClient, added interface

---
+++ Client/BannerzApiClient.cs
@@ -0,0 +1,52 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public interface IBannerzApiClient
+    {
+        BannerDTO GetAsync(int id);
+
+        BannerDTO PostAsync(BannerDTO banner);
+
+        BannerDTO PutAsync(int id, BannerDTO banner);
+
+        BannerDTO DeleteAsync(int id);
+    }
+
+    public class BannerzApiClient : IBannerzApiClient
+    {
+        private HttpClient _client;
+
+        public BannerzApiClient(string baseUrl)
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = new Uri(baseUrl);
+        }
+
+        BannerDTO IBannerzApiClient.DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        BannerDTO IBannerzApiClient.GetAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        BannerDTO IBannerzApiClient.PostAsync(BannerDTO banner)
+        {
+            throw new NotImplementedException();
+        }
+
+        BannerDTO IBannerzApiClient.PutAsync(int id, BannerDTO banner)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
