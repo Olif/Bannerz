23df3ccfae0199bc8302df21af9d2f1ef39a43df
Niklas Olofsson <niklas.olofsson@olif.nu>
2016-08-20 14:01:22 +0200

    Added client project

---
+++ Client/BannersClient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class BannersClient
+    {
+        private HttpClient _client;
+
+        public BannersClient(string baseUrl)
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = new Uri(baseUrl);
+        }
+    }
+}
